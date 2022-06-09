82fbaa962c1f1fed2e435740717765420351c1c2
jknotts <kirill.klimov.94@gmail.com>
2022-06-09 22:28:12 +0300

    chan learn

---
+++ twochan.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func square(c chan int) {
+	fmt.Println("[square] reading")
+	num := <-c
+	c <- num * num
+}
+
+func cube(c chan int) {
+	fmt.Println("[cube] reading")
+	num := <-c
+	c <- num * num * num
+}
+
+func main() {
+	fmt.Println("[main] main() started")
+
+	squareChan := make(chan int)
+	cubeChan := make(chan int)
+
+	go square(squareChan)
+	go cube(cubeChan)
+
+	testNum := 3
+
+	fmt.Println("[main] sent testNum to squareChan")
+	squareChan <- testNum
+	fmt.Println("[main] resuming")
+
+	fmt.Println("[main] sent testNumber to cubeChan")
+	cubeChan <- testNum
+	fmt.Println("[main] resuming")
+
+	fmt.Println("[main] reading from channals")
+	squareVal, cubeVal := <-squareChan, <-cubeChan
+	fmt.Printf("squareVal - [%d], cubeVal - [%d]\n", squareVal, cubeVal)
+
+	sum := squareVal + cubeVal
+	fmt.Println("[main] sun of square and cube of", testNum, " is", sum)
+	fmt.Println("[main] main() stopped")
+
+}
